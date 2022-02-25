1c7ab9ec4f1d4a4a86628f2aed8a76f6c891c147
João Pedro Mayrink <joaopmayrinkj@gmail.com>
2022-02-25 13:48:57 -0300

    feat: create manage-properties page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { ManagePropertiesComponent } from './pages/manage-properties/manage-properties.component';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     component: LandingPageComponent,
+  },
+  {
+    path: 'gerenciar-imoveis',
+    component: ManagePropertiesComponent,
   }
 ];
 
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ComponentsModule } from '../core/components/components.module';
+import { ManagePropertiesComponent } from './manage-properties/manage-properties.component';
 
 
 
 @NgModule({
   declarations: [
     LandingPageComponent,
+    ManagePropertiesComponent,
   ],
   imports: [
     CommonModule,
