50149b78b2029197bfe88267c8d06b41d0252cb8
João Pedro Mayrink <joaopmayrinkj@gmail.com>
2022-02-25 13:54:16 -0300

    feat: create and implement footer-component

--- src/app/core/components/components.module.ts
+++ src/app/core/components/components.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     CommonModule,
@@ -15,6 +17,7 @@ import { RouterModule } from '@angular/router';
   ],
   exports: [
     HeaderComponent,
+    FooterComponent,
   ]
 })
 export class ComponentsModule { }
