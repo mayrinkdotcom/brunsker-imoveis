440a85d75d81e8d8adb55f7383dd710fd42388a4
João Pedro Mayrink <joaopmayrinkj@gmail.com>
2022-03-06 00:52:57 -0300

    fix: import HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
+
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { PagesModule } from './pages/pages.module';
     BrowserModule,
     AppRoutingModule,
     PagesModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
