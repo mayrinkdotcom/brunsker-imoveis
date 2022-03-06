96c9f26340b761cfed4755cf3af6341ea775b37d
João Pedro Mayrink <joaopmayrinkj@gmail.com>
2022-03-06 01:00:05 -0300

    feat: get properties from JSON Server

--- src/app/pages/manage-properties/manage-properties.component.ts
+++ src/app/pages/manage-properties/manage-properties.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { PropertiesService } from 'src/app/services/properties.service';
 
 @Component({
   selector: 'app-manage-properties',
   templateUrl: './manage-properties.component.html',
   styleUrls: ['./manage-properties.component.scss']
 })
-export class ManagePropertiesComponent implements OnInit {
+export class ManagePropertiesComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  properties: any = [];
+
+  subscriptions: Subscription[] = [];
+
+  constructor(
+    private propertiesService: PropertiesService,
+  ) { }
 
   ngOnInit(): void {
+    try {
+      const sub = this.propertiesService.getAllProperties().subscribe(data => {
+        if (!!data) {
+          this.properties = data;
+          console.log("🚀 -> ManagePropertiesComponent -> ngOnInit -> res", this.properties);
+        }
+      });
+
+      this.subscriptions.push(sub);
+    } catch (error) {
+      console.error('ERROR on ManagePropertiesComponent');
+      throw error;
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => {
+      sub.unsubscribe();
+    });
   }
 
 }
